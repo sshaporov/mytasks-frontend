6e2c768f4dea7630f45fbcdc533d56723ecf4510
Sergey Shaporov <sshaporov86@gmail.com>
2021-01-08 00:33:30 +0300

    add React.memo

--- src/components/cardTasks/cardFooter/CardFooter.tsx
+++ src/components/cardTasks/cardFooter/CardFooter.tsx
@@ -1,9 +1,10 @@
 import {DEV_VERSION} from '../../../config'
+import React from 'react'
 
-export const CardFooter = () => {
+export const CardFooter = React.memo(() => {
   DEV_VERSION && console.log('CardFooter')
 
   return (
    <div>CardFooter</div>
   )
-}
+})
--- src/components/cardTasks/cardHeader/CardHeader.tsx
+++ src/components/cardTasks/cardHeader/CardHeader.tsx
@@ -1,9 +1,10 @@
 import {DEV_VERSION} from '../../../config'
+import React from 'react'
 
-export const CardHeader = () => {
+export const CardHeader = React.memo(() => {
   DEV_VERSION && console.log('CardFooter')
 
   return (
    <div>CardHeader</div>
   )
-}
+})
