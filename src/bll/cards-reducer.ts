e62706d42eb0e959cffb418f57e1d5f561a090fd
Sergey Shaporov <sshaporov86@gmail.com>
2021-01-11 18:38:18 +0300

    impl cards reducer

--- src/bll/cards-reducer.ts
+++ src/bll/cards-reducer.ts
@@ -1,28 +1,55 @@
+import {v1} from 'uuid';
+
 enum ACTIONS_TYPE {
   ADD_CARD = 'Cards/ADD_CARD',
   REMOVE_CARD = 'Cards/REMOVE_CARD',
+  CHANGE_CARD_TITLE = 'Card/CHANGE_CARD_TITLE',
+  CHANGE_CARD_FILTER = 'Card/CHANGE_CARD_FILTER'
 }
 
+export type CardFilterType = 'ALL' | 'ACTIVE' | 'DONE'
 export type CardType = {
   id: string
   title: string
   filter: string
 }
-const initialState: Array<CardType> =  []
+const initialState: Array<CardType> = []
 
 export const cardsReducer = (state: Array<CardType> = initialState, action: CardsACType): Array<CardType> => {
-  switch (action.type) {
+  switch (action.type){
+    case ACTIONS_TYPE.ADD_CARD:
+      return [...state, {id: action.cardId, title: action.cardTitle, filter: 'ALL'}]
+    case ACTIONS_TYPE.CHANGE_CARD_TITLE: {
+      const card = state.find(card => card.id === action.cardId)
+      if (card) {
+        card.title = action.cardTitle
+      }
+    }
+      return [...state]
     case ACTIONS_TYPE.REMOVE_CARD:
       return state.filter(c => c.id !== action.cardId)
+    case ACTIONS_TYPE.CHANGE_CARD_FILTER: {
+      const card = state.find(card => card.id === action.cardId)
+      if (card) {
+        card.filter = action.filter
+      }
+    }
+    return [...state]
+    default:
+      return state
   }
 }
 
 // actions
-export const addCard = (card: CardType) => ({type: ACTIONS_TYPE.ADD_CARD, card} as const)
+export const addCard = (cardTitle: string) => ({type: ACTIONS_TYPE.ADD_CARD, cardTitle, cardId: v1()} as const)
+export const changeCardTitle = (cardId: string, cardTitle: string) => ({type: ACTIONS_TYPE.CHANGE_CARD_TITLE, cardId, cardTitle} as const)
 export const removeCard = (cardId: string) => ({type: ACTIONS_TYPE.REMOVE_CARD, cardId} as const)
+export const changeCardFilter = (cardId: string, filter: CardFilterType) => ({type: ACTIONS_TYPE.CHANGE_CARD_FILTER, cardId, filter} as const)
 
 // types
+export type AddCardACType = ReturnType<typeof addCard>
+export type ChangeCardTitleACType = ReturnType<typeof changeCardTitle>
 export type RemoveCardACType = ReturnType<typeof removeCard>
-export type AddCardACType = ReturnType<typeof removeCard>
-export type CardsACType = RemoveCardACType | AddCardACType
+export type ChangeCardFilterACType = ReturnType<typeof changeCardFilter>
+export type CardsACType = AddCardACType | ChangeCardTitleACType | RemoveCardACType | ChangeCardFilterACType
 
