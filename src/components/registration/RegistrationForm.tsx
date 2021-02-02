fe27a91033ff1f6b2dc2d04ed89d52bd0aced803
Sergey Shaporov <sshaporov86@gmail.com>
2021-02-02 22:00:48 +0300

    refactoring registration form

--- src/components/registration/RegistrationForm.tsx
+++ src/components/registration/RegistrationForm.tsx
@@ -48,91 +48,96 @@ export const RegistrationForm = () => {
   }
 
   return (
+    <div className='wrapper-registrationForm'>
+      <div className='content-registrationForm'>
+        <Form
+          {...layout}
+          name='register'
+          className='registration-form'
+          onFinish={onFinish}
+        >
+          <div className='title-registrationForm'>
+            Registration
+          </div>
+          <Form.Item
+            name='name'
+            label='Name'
+          >
+            <Input/>
+          </Form.Item>
 
-    <Form
-      {...layout}
-      name='register'
-      className='registration-form'
-      onFinish={onFinish}
-    >
-      <Form.Item
-        name='name'
-        label='Name'
-      >
-        <Input/>
-      </Form.Item>
+          <Form.Item
+            name='email'
+            label='Email'
+            rules={[
+              {
+                type: 'email',
+                message: 'The input is not valid E-mail!',
+              },
+              {
+                required: true,
+                message: 'Please input your E-mail!',
+              },
+            ]}
+          >
+            <Input/>
+          </Form.Item>
 
-      <Form.Item
-        name='email'
-        label='Email'
-        rules={[
-          {
-            type: 'email',
-            message: 'The input is not valid E-mail!',
-          },
-          {
-            required: true,
-            message: 'Please input your E-mail!',
-          },
-        ]}
-      >
-        <Input/>
-      </Form.Item>
+          <Form.Item
+            name='password'
+            label='Password'
+            rules={[
+              {
+                required: true,
+                message: 'Please input your password!',
+              },
+              {
+                min: 3,
+                message: 'Password should be more than 3 symbols!',
+              },
+            ]}
+            hasFeedback
+          >
+            <Input.Password autoComplete='on'/>
+          </Form.Item>
 
-      <Form.Item
-        name='password'
-        label='Password'
-        rules={[
-          {
-            required: true,
-            message: 'Please input your password!',
-          },
-          {
-            min: 3,
-            message: 'Password should be more than 3 symbols!',
-          },
-        ]}
-        hasFeedback
-      >
-        <Input.Password autoComplete='on'/>
-      </Form.Item>
+          <Form.Item
+            name='confirm'
+            label='Confirm Password'
+            dependencies={['password']}
+            hasFeedback
+            rules={[
+              {
+                required: true,
+                message: 'Please confirm your password!',
+              },
+              {
+                min: 3,
+                message: 'Password should be more than 3 symbols!',
+              },
 
-      <Form.Item
-        name='confirm'
-        label='Confirm Password'
-        dependencies={['password']}
-        hasFeedback
-        rules={[
-          {
-            required: true,
-            message: 'Please confirm your password!',
-          },
-          {
-            min: 3,
-            message: 'Password should be more than 3 symbols!',
-          },
+              ({getFieldValue}) => ({
+                validator(_, value) {
+                  if (!value || getFieldValue('password') === value) {
+                    return Promise.resolve()
+                  }
 
-          // вынести отдельно и обернуть в useCallback
-          ({getFieldValue}) => ({
-            validator(_, value) {
-              if (!value || getFieldValue('password') === value) {
-                return Promise.resolve();
-              }
+                  return Promise.reject('The two passwords that you entered do not match!')
+                }
+              })
+            ]}
+          >
+            <Input.Password autoComplete='on'/>
+          </Form.Item>
 
-              return Promise.reject('The two passwords that you entered do not match!');
-            },
-          }),
-        ]}
-      >
-        <Input.Password autoComplete='on'/>
-      </Form.Item>
+          <Form.Item {...tailLayout}>
+            <Button type='primary' htmlType='submit'>
+              Register
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
 
-      <Form.Item {...tailLayout}>
-        <Button type='primary' htmlType='submit'>
-          Register
-        </Button>
-      </Form.Item>
-
-    </Form>
   )
 }
