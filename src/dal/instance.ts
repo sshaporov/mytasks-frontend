8368d076f09aa882ebb139fc2ba9811cf6a26a6b
Sergey Shaporov <sshaporov86@gmail.com>
2021-02-06 14:41:57 +0300

    upd app favicon

--- src/dal/instance.ts
+++ src/dal/instance.ts
@@ -1,5 +1,13 @@
 import axios from 'axios'
 
 export const instance = axios.create({
-  baseURL: 'https://localhost:3010',
+  // 1. add script: "start": "HTTPS=true react-scripts start" to package.json for using HTTPS protocol
+  // 2. and need to uncomment some code on the server side (see mytasks-backend repo)
+  // baseURL: 'https://localhost:3010',
+
+  // local URL nodeJS server
+  // baseURL: 'http://localhost:3010',
+
+  // Heroku URL nodeJS server
+  baseURL: 'https://fierce-falls-39397.herokuapp.com'
 })
